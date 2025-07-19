bd9e492c76560f70b5ee638a925f8612605d0c6f
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-07-19 05:36:43 -0400

    formatting

--- stalker-gamma.core/Services/GammaInstaller/AddonsAndSeparators/AddonsAndSeparators.cs
+++ stalker-gamma.core/Services/GammaInstaller/AddonsAndSeparators/AddonsAndSeparators.cs
@@ -81,9 +81,17 @@ public partial class AddonsAndSeparators(ProgressService progressService, ModDb 
                         $"_______________ {downloadableRecord.AddonName} _______________"
                     );
                     await downloadableRecord.DownloadAsync(downloadsPath, useCurlImpersonate);
-                    if (await downloadableRecord.ShouldDownloadAsync(downloadsPath, checkMd5, forceGitDownload))
+                    if (
+                        await downloadableRecord.ShouldDownloadAsync(
+                            downloadsPath,
+                            checkMd5,
+                            forceGitDownload
+                        )
+                    )
                     {
-                        progressService.UpdateProgress($"Md5 mismatch in downloaded file: {downloadableRecord.DlPath}. Downloading again.");
+                        progressService.UpdateProgress(
+                            $"Md5 mismatch in downloaded file: {downloadableRecord.DlPath}. Downloading again."
+                        );
                         await downloadableRecord.DownloadAsync(downloadsPath, useCurlImpersonate);
                     }
                     extract = true;
