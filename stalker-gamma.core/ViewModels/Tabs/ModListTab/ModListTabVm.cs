a1e2f67ecd3438438e18f83b84a8cdb8ddf716c4
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-07-30 18:58:39 -0400

    wip

--- stalker-gamma.core/ViewModels/Tabs/ModListTab/ModListTabVm.cs
+++ stalker-gamma.core/ViewModels/Tabs/ModListTab/ModListTabVm.cs
@@ -10,14 +10,11 @@ namespace stalker_gamma.core.ViewModels.Tabs.ModListTab;
 public class ModListTabVm : ViewModelBase, IActivatableViewModel
 {
     private readonly string _dir = Path.GetDirectoryName(AppContext.BaseDirectory)!;
-    private readonly ReadOnlyObservableCollection<string> _modsList;
 
     public ModListTabVm(ProgressService progressService)
     {
         Activator = new ViewModelActivator();
         var mo2ModsFile = Path.Join(_dir, "..", "profiles", "G.A.M.M.A", "modlist.txt");
-        SourceList<string> modsSourceList = new();
-        modsSourceList.Connect().Bind(out _modsList).Subscribe();
         GetModListCmd = ReactiveCommand.CreateFromTask(async () =>
         {
             if (!File.Exists(mo2ModsFile))
@@ -29,11 +26,8 @@ public class ModListTabVm : ViewModelBase, IActivatableViewModel
             var modsList = (await File.ReadAllLinesAsync(mo2ModsFile)).Where(x =>
                 x.StartsWith('-') || x.StartsWith('+')
             );
-            modsSourceList.Edit(inner =>
-            {
-                inner.Clear();
-                inner.AddRange(modsList);
-            });
+            ModsList = [];
+            foreach (var mod in modsList) { }
         });
 
         this.WhenActivated(
@@ -46,6 +40,6 @@ public class ModListTabVm : ViewModelBase, IActivatableViewModel
 
     public ReactiveCommand<Unit, Unit> GetModListCmd { get; }
 
-    public ReadOnlyObservableCollection<string> ModsList => _modsList;
+    public ObservableCollection<ModNode> ModsList { get; set; }
     public ViewModelActivator Activator { get; }
 }
---
+++ stalker-gamma.core/ViewModels/Tabs/ModListTab/ModNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace stalker_gamma.core.ViewModels.Tabs.ModListTab;
+
+public class ModNode
+{
+    public ObservableCollection<ModNode>? SubNodes { get; }
+    public string Title { get; }
+
+    public ModNode(string title)
+    {
+        Title = title;
+    }
+
+    public ModNode(string title, ObservableCollection<ModNode> subNodes)
+    {
+        Title = title;
+        SubNodes = subNodes;
+    }
+}
