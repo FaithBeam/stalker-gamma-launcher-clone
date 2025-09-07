e38c08efca6670f859acb3cd223dd71f07dab2ad
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-09-07 13:44:22 -0400

    include missing backup vm

---
+++ stalker-gamma.core/ViewModels/Tabs/BackupTab/BackupService.cs
@@ -0,0 +1,83 @@
+﻿using System.Reactive.Linq;
+using CliWrap.EventStream;
+using stalker_gamma.core.Utilities;
+
+namespace stalker_gamma.core.ViewModels.Tabs.BackupTab;
+
+public enum Compressor
+{
+    Lzma2,
+    Zstd
+}
+
+public enum CompressionLevel
+{
+    None,
+    Fast,
+    Max
+}
+
+public record BackupSettings(CompressionLevel CompressionLevel, Compressor Compressor);
+
+public class BackupService
+{
+    private readonly BackupTabProgressService _progress;
+
+    private readonly string _backupPath =
+        Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "GAMMA", "Backups");
+
+    public BackupService(BackupTabProgressService progress)
+    {
+        _progress = progress;
+        if (!Directory.Exists(_backupPath))
+        {
+            Directory.CreateDirectory(_backupPath);
+        }
+    }
+
+    public List<string> GetBackups() => Directory.Exists(_backupPath) ? Directory.GetFiles(_backupPath).ToList() : [];
+
+    public async Task Backup(BackupSettings backupSettings)
+    {
+        await ArchiveUtility.Archive([@"C:\anomaly", @"C:\gamma"],
+                Path.Join(_backupPath, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")),
+                backupSettings.Compressor.ToString().ToLower(),
+                GetCompressionLevel(backupSettings.Compressor, backupSettings.CompressionLevel), ["downloads"])
+            .ForEachAsync(cmdEvent =>
+            {
+                switch (cmdEvent)
+                {
+                    case StartedCommandEvent started:
+                        break;
+                    case StandardOutputCommandEvent standardOutput:
+                        _progress.UpdateProgress(standardOutput.Text);
+                        break;
+                    case StandardErrorCommandEvent standardError:
+                        _progress.UpdateProgress(standardError.Text);
+                        break;
+                    case ExitedCommandEvent exited:
+                        break;
+                }
+            });
+    }
+
+    private static string GetCompressionLevel(Compressor compressor, CompressionLevel compressionLevel) =>
+        compressor switch
+        {
+            Compressor.Lzma2 => compressionLevel switch
+            {
+                CompressionLevel.None => "0",
+                CompressionLevel.Fast => "1",
+                CompressionLevel.Max => "9",
+                _ => throw new ArgumentOutOfRangeException(nameof(compressionLevel), compressionLevel, null)
+            },
+            Compressor.Zstd => compressionLevel switch
+            {
+                CompressionLevel.None => "0",
+                CompressionLevel.Fast => "1",
+                CompressionLevel.Max => "22",
+                _ => throw new ArgumentOutOfRangeException(nameof(compressionLevel), compressionLevel, null)
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(compressor), compressor, null)
+        };
+}
---
+++ stalker-gamma.core/ViewModels/Tabs/BackupTab/BackupTabVm.cs
@@ -0,0 +1,70 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace stalker_gamma.core.ViewModels.Tabs.BackupTab;
+
+public class BackupTabVm : ViewModelBase
+{
+    private readonly BackupService _backupService;
+    private readonly BackupTabProgressService _backupTabProgressService;
+    private Compressor _selectedCompressor;
+    private CompressionLevel _selectedCompressionLevel;
+    private readonly ObservableAsPropertyHelper<string?> _estimates;
+
+    public BackupTabVm(BackupService backupService, BackupTabProgressService backupTabProgressService)
+    {
+        _backupService = backupService;
+        _backupTabProgressService = backupTabProgressService;
+        _selectedCompressor = Compressors.First(x => x == Compressor.Lzma2);
+        _selectedCompressionLevel = CompressionLevels.First(x => x == CompressionLevel.Fast);
+        BackupCmd = ReactiveCommand.CreateFromTask(() =>
+            _backupService.Backup(new BackupSettings(SelectedCompressionLevel, SelectedCompressor)));
+
+        AppendLineInteraction = new Interaction<string, Unit>();
+        backupTabProgressService.BackupProgressObservable.ObserveOn(RxApp.MainThreadScheduler).Select(x => x.Message)
+            .WhereNotNull().Subscribe(async x => await AppendLineInteraction.Handle(x));
+
+        _estimates = this
+            .WhenAnyValue(x => x.SelectedCompressor, x => x.SelectedCompressionLevel,
+                selector: (selComp, selLevel) => selComp switch
+                {
+                    Compressor.Lzma2 => selLevel switch
+                    {
+                        CompressionLevel.None => "changeme",
+                        CompressionLevel.Fast => "changeme",
+                        CompressionLevel.Max => "changeme",
+                        _ => throw new ArgumentOutOfRangeException(nameof(selLevel), selLevel, null)
+                    },
+                    Compressor.Zstd => selLevel switch
+                    {
+                        CompressionLevel.None => "changeme",
+                        CompressionLevel.Fast => "≈ 10 minutes, 65gb 7800X3D CPU",
+                        CompressionLevel.Max => "changeme",
+                        _ => throw new ArgumentOutOfRangeException(nameof(selLevel), selLevel, null)
+                    },
+                    _ => throw new ArgumentOutOfRangeException(nameof(selComp), selComp, null)
+                }).ToProperty(this, x => x.Estimates);
+    }
+
+    public Interaction<string, Unit> AppendLineInteraction { get; }
+    public ReactiveCommand<Unit, Unit> BackupCmd { get; }
+    public IReadOnlyList<Compressor> Compressors { get; } = [Compressor.Lzma2, Compressor.Zstd];
+
+    public Compressor SelectedCompressor
+    {
+        get => _selectedCompressor;
+        set => this.RaiseAndSetIfChanged(ref _selectedCompressor, value);
+    }
+
+    public IReadOnlyList<CompressionLevel> CompressionLevels { get; } =
+        [CompressionLevel.None, CompressionLevel.Fast, CompressionLevel.Max];
+
+    public CompressionLevel SelectedCompressionLevel
+    {
+        get => _selectedCompressionLevel;
+        set => this.RaiseAndSetIfChanged(ref _selectedCompressionLevel, value);
+    }
+
+    public string? Estimates => _estimates.Value;
+}
