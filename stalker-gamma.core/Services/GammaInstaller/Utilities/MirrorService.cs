84e1a3a4f07bc18b9a78736884dfe8abe0840cc1
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-11-22 07:40:09 -0500

    change mirror logic to only retrieve mirrors once and reuse that response from stalker-api. More random mirror selection.

--- stalker-gamma.core/Services/GammaInstaller/Utilities/MirrorService.cs
+++ stalker-gamma.core/Services/GammaInstaller/Utilities/MirrorService.cs
@@ -4,19 +4,18 @@ public class MirrorService(IHttpClientFactory hcf)
 {
     private readonly IHttpClientFactory _hcf = hcf;
     private const string MirrorUrl = "https://stalker-gamma.com/api/mirrors";
+    private string? _mirrors;
 
     public async Task<string?> GetMirror()
     {
         var hc = _hcf.CreateClient();
-        var content = await hc.GetStringAsync(MirrorUrl);
+        _mirrors ??= await hc.GetStringAsync(MirrorUrl);
 
-        var lines = content.Split('\n');
+        var lines = _mirrors.Split('\n');
         var random = new Random();
 
         return lines
             .Select(line => new { Line = line, Parts = line.Split('\t') })
-            .OrderBy(x => double.Parse(x.Parts[^2]))
-            .Take(3)
             .OrderBy(_ => random.Next())
             .FirstOrDefault()
             ?.Line.Split('\t')[1];
@@ -25,16 +24,14 @@ public class MirrorService(IHttpClientFactory hcf)
     public async Task<string?> GetMirror(params string[] excludeMirrors)
     {
         var hc = _hcf.CreateClient();
-        var content = await hc.GetStringAsync(MirrorUrl);
+        _mirrors ??= await hc.GetStringAsync(MirrorUrl);
 
-        var lines = content.Split('\n');
+        var lines = _mirrors.Split('\n');
         var random = new Random();
 
         return lines
             .Where(line => excludeMirrors.All(em => !line.Contains(em)))
             .Select(line => new { Line = line, Parts = line.Split('\t') })
-            .OrderBy(x => double.Parse(x.Parts[^2]))
-            .Take(3)
             .OrderBy(_ => random.Next())
             .FirstOrDefault()
             ?.Line.Split('\t')[1];
