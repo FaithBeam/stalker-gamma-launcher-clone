fd1d58a9080ff3690172384b1d644c3bcefc7f2d
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-11-28 15:51:18 -0500

    add a coloring to the background of rows that aren't mods

---
+++ stalker-gamma-gui/Converters/DataGridRowBackgroundColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using stalker_gamma.core.Services.GammaInstaller.AddonsAndSeparators.Models;
+using stalker_gamma.core.ViewModels.Tabs.MainTab;
+
+namespace stalker_gamma_gui.Converters;
+
+public class DataGridRowBackgroundColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not ModDownloadExtractProgressVm vm)
+        {
+            return null;
+        }
+
+        if (vm.ModListRecord is GitRecord or ModpackSpecific)
+        {
+            return Brush.Parse("#0F110C");
+        }
+
+        return Brush.Parse("Black");
+    }
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
