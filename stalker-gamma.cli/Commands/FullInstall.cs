65931f380002bcae03d357d3cc8427f56d0c09b8
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-11-29 14:24:02 -0500

    initial cli work

---
+++ stalker-gamma.cli/Commands/FullInstall.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleAppFramework;
+
+namespace stalker_gamma.cli.Commands;
+
+[RegisterCommands]
+public class FullInstallCmd
+{
+    /// <summary>
+    /// This will install/update all mods based on Stalker_GAMMA
+    /// </summary>
+    /// <param name="anomaly">Anomaly path</param>
+    /// <param name="gamma">GAMMA path</param>
+    /// <param name="cacheDirectory">cache directory</param>
+    public void FullInstall(string anomaly, string gamma, string? cacheDirectory = null) =>
+        Console.WriteLine("Full Install");
+}
