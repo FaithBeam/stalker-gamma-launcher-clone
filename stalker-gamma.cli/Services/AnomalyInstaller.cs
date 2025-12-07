f30c8d84409a9453aca1c7eb6c2607442879e353
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-12-07 15:37:54 -0500

    missed two

---
+++ stalker-gamma.cli/Services/AnomalyInstaller.cs
@@ -0,0 +1,26 @@
+﻿using stalker_gamma.core.Services.GammaInstaller.Utilities;
+using stalker_gamma.core.Utilities;
+
+namespace stalker_gamma.cli.Services;
+
+public class AnomalyInstaller(ModDb modDb)
+{
+    public async Task DownloadAndExtractAsync(
+        string archivePath,
+        string extractDirectory,
+        Action<double> onProgress
+    )
+    {
+        // download anomaly if archive does not exist
+        if (!File.Exists(archivePath))
+        {
+            await _modDb.GetModDbLinkCurl(AnomalyMdbUrl, archivePath, onProgress);
+        }
+
+        // extract anomaly
+        await ArchiveUtility.ExtractWithProgress(archivePath, extractDirectory, onProgress);
+    }
+
+    private const string AnomalyMdbUrl = "https://www.moddb.com/downloads/start/277404";
+    private readonly ModDb _modDb = modDb;
+}
