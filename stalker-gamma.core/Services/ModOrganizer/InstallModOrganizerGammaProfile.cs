4498f6115529a87409631389a95bd907341f0c10
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2025-12-22 07:49:33 -0500

    Add Serilog for logging in `InstallModOrganizerGammaProfile` and update `csproj` with the Serilog package reference.

--- stalker-gamma.core/Services/ModOrganizer/InstallModOrganizerGammaProfile.cs
+++ stalker-gamma.core/Services/ModOrganizer/InstallModOrganizerGammaProfile.cs
@@ -1,12 +1,14 @@
+using Serilog;
+
 namespace stalker_gamma.core.Services.ModOrganizer;
 
-public class InstallModOrganizerGammaProfile
+public class InstallModOrganizerGammaProfile(ILogger logger)
 {
     public async Task InstallAsync(string stalkerGammaRepoPath, string gammaPath)
     {
         var gammaProfilesPath = Path.Join(gammaPath, "profiles", "G.A.M.M.A");
         var settingsPath = Path.Join(gammaProfilesPath, "settings.txt");
-        Console.WriteLine($"[+] Installing G.A.M.M.A profile in {gammaProfilesPath}");
+        logger.Information("Installing G.A.M.M.A profile in {Path}", gammaProfilesPath);
         Directory.CreateDirectory(gammaProfilesPath);
         File.Copy(
             Path.Join(stalkerGammaRepoPath, "G.A.M.M.A", "modpack_data", "modlist.txt"),
