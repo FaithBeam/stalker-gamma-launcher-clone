c1d9466bb2110c1f8d93b4ef4bf11bdc81a1f375
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-01-03 16:27:53 -0500

    stalker-gamma-cli `update check` and `update apply`

--- stalker-gamma.cli/Commands/Update.cs
+++ stalker-gamma.cli/Commands/Update.cs
@@ -1,3 +1,4 @@
+using System.Reactive.Linq;
 using System.Text.Json;
 using ConsoleAppFramework;
 using Serilog;
@@ -10,17 +11,18 @@ using Stalker.Gamma.Models;
 
 namespace stalker_gamma.cli.Commands;
 
-[RegisterCommands]
+[RegisterCommands("update")]
 public class UpdateCmds(
     ILogger logger,
     CliSettings cliSettings,
     StalkerGammaSettings stalkerGammaSettings,
     IGetStalkerModsFromApi getStalkerModsFromApi,
-    IModListRecordFactory modListRecordFactory
+    IModListRecordFactory modListRecordFactory,
+    GammaInstaller gammaInstaller
 )
 {
-    [Command("update")]
-    public async Task ListUpdates()
+    [Command("check")]
+    public async Task CheckUpdates()
     {
         ValidateActiveProfile.Validate(_logger, _cliSettings.ActiveProfile);
         stalkerGammaSettings.ModpackMakerList = _cliSettings.ActiveProfile!.ModPackMakerUrl;
@@ -97,12 +99,100 @@ public class UpdateCmds(
                     );
                 }
             }
+
+            _logger.Information("To apply updates, run `stalker-gamma update apply`");
         }
         else
         {
             _logger.Information("No updates found");
         }
     }
+
+    public async Task Apply(
+        CancellationToken cancellationToken,
+        bool verbose = false,
+        [Hidden] string? mo2Version = null,
+        [Hidden] long progressUpdateIntervalMs = 250
+    )
+    {
+        ValidateActiveProfile.Validate(_logger, _cliSettings.ActiveProfile);
+        stalkerGammaSettings.ModpackMakerList = _cliSettings.ActiveProfile!.ModPackMakerUrl;
+
+        var anomaly = _cliSettings.ActiveProfile!.Anomaly;
+        var gamma = _cliSettings.ActiveProfile!.Gamma;
+        var cache = _cliSettings.ActiveProfile!.Cache;
+        var mo2Profile = _cliSettings.ActiveProfile!.Mo2Profile;
+        var modpackMakerUrl = _cliSettings.ActiveProfile!.ModPackMakerUrl;
+        var modListUrl = _cliSettings.ActiveProfile!.ModListUrl;
+        stalkerGammaSettings.DownloadThreads = _cliSettings.ActiveProfile!.DownloadThreads;
+        stalkerGammaSettings.ModpackMakerList = modpackMakerUrl;
+        stalkerGammaSettings.ModListUrl = modListUrl;
+
+        var resourcesPath = Path.Join(Path.GetDirectoryName(AppContext.BaseDirectory), "resources");
+        stalkerGammaSettings.PathToCurl = Path.Join(
+            resourcesPath,
+            OperatingSystem.IsWindows() ? "curl.exe" : "curl-impersonate"
+        );
+        stalkerGammaSettings.PathTo7Z = Path.Join(
+            resourcesPath,
+            OperatingSystem.IsWindows() ? "7zz.exe" : "7zz"
+        );
+        stalkerGammaSettings.PathToGit = OperatingSystem.IsWindows()
+            ? Path.Join(resourcesPath, "git", "cmd", "git.exe")
+            : "git";
+
+        var gammaProgressObservable = Observable
+            .FromEventPattern<GammaProgress.GammaInstallProgressEventArgs>(
+                handler => gammaInstaller.Progress.ProgressChanged += handler,
+                handler => gammaInstaller.Progress.ProgressChanged -= handler
+            )
+            .Select(x => x.EventArgs);
+        var gammaProgressDisposable = gammaProgressObservable
+            .Sample(TimeSpan.FromMilliseconds(progressUpdateIntervalMs))
+            .Subscribe(verbose ? OnProgressChangedVerbose : OnProgressChangedInformational);
+        try
+        {
+            await gammaInstaller.UpdateAsync(
+                new InstallUpdatesArgs
+                {
+                    Gamma = gamma,
+                    Anomaly = anomaly,
+                    Cache = cache,
+                    CancellationToken = cancellationToken,
+                    Mo2Profile = mo2Profile,
+                    Mo2Version = mo2Version,
+                }
+            );
+            _logger.Information("Update finished");
+        }
+        finally
+        {
+            gammaProgressDisposable.Dispose();
+        }
+    }
+
+    private void OnProgressChangedInformational(GammaProgress.GammaInstallProgressEventArgs e) =>
+        _logger.Information(
+            Informational,
+            e.Name[..Math.Min(e.Name.Length, 35)].PadRight(40),
+            e.ProgressType.PadRight(10),
+            $"{e.Progress:P2}".PadRight(8),
+            $"[{e.Complete}/{e.Total}]"
+        );
+
+    private void OnProgressChangedVerbose(GammaProgress.GammaInstallProgressEventArgs e) =>
+        _logger.Information(
+            Verbose,
+            e.Name[..Math.Min(e.Name.Length, 35)].PadRight(40),
+            e.ProgressType.PadRight(10),
+            $"{e.Progress:P2}".PadRight(8),
+            $"[{e.Complete}/{e.Total}]",
+            e.Url
+        );
+
+    private const string Informational = "{AddonName} | {Operation} | {Percent} | {CompleteTotal}";
+    private const string Verbose =
+        "{AddonName} | {Operation} | {Percent} | {CompleteTotal} | {Url}";
 
     private readonly ILogger _logger = logger;
     private readonly CliSettings _cliSettings = cliSettings;
